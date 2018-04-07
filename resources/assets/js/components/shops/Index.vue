71c2844f2964ec9625764feda564a51d52553846
kingdle <nkings@163.com>
2018-04-08 04:01:58 +0800

    update the ProfileWrapper.vue

--- resources/assets/js/components/shops/Index.vue
+++ resources/assets/js/components/shops/Index.vue
@@ -2,7 +2,7 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card card-default">
-                <div class="card-header">Home Page</div>
+                <div class="card-header">首页</div>
                 <div v-for="shop in shops" :key="shop.id" class="card-body">
                     <h4>
                         <router-link :to="{name: 'shops', params: {id: shop.id}}">{{shop.title}}</router-link>
