4c9ac0b51085ceab7ba4aa326598aba2c351e82d
kingdle <nkings@163.com>
2018-05-03 23:26:48 +0800

    UserCollection phone no replace

--- app/Http/Resources/User.php
+++ app/Http/Resources/User.php
@@ -9,20 +9,22 @@ class User extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
+     *
      * @return array
      */
     public function toArray($request)
     {
         return [
-            'id'=>$this->id,
-            'name'=>$this->name,
-            'phone'=>substr_replace($this->phone,'****',3,4),
-            'email'=>$this->email,
+            'id' => $this->id,
+            'name' => $this->name,
+//            'phone'=>substr_replace($this->phone,'****',3,4),
+            'phone' => $this->phone,
+            'email' => $this->email,
 //            'avatar'=>$this->avatar,
-            'avatar_url'=>$this->avatar_url,
-            'is_active'=>$this->is_active,
-            'is_admin'=>$this->is_admin,
+            'avatar_url' => $this->avatar_url,
+            'is_active' => $this->is_active,
+            'is_admin' => $this->is_admin,
 //            'weapp_openid'=>$this->weapp_openid,
 //            'weixin_session_key'=>$this->weixin_session_key,
 //            'shop'=>new Shop($this->shop)
