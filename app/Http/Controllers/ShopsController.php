7c943bbe7e5f557da054b9dcce0311d17ae77138
kingdle <nkings@163.com>
2018-05-05 11:15:53 +0800

    add shop API distance

--- app/Http/Controllers/ShopsController.php
+++ app/Http/Controllers/ShopsController.php
@@ -17,39 +17,10 @@ class ShopsController extends Controller
         return new ShopCollection($shops);
     }
 
-    public function distance(Request $request, Shop $shop)
+    public function distance(Request $request)
     {
-        /**
-         * 计算两点地理坐标之间的距离
-         * @param  Decimal $longitude1 起点经度
-         * @param  Decimal $latitude1  起点纬度
-         * @param  Decimal $longitude2 终点经度
-         * @param  Decimal $latitude2  终点纬度
-         * @param  Int     $unit       单位 1:米 2:公里
-         * @param  Int     $decimal    精度 保留小数位数
-         * @return Decimal */
-        $unit = 2;
-        $decimal = 0;
-        $latitude1 = $request->latitude;
-        $latitude2 = Auth::guard('api')->user()->shop->latitude;
-        $longitude1 = $request->longitude;
-        $longitude2 = Auth::guard('api')->user()->shop->longitude;
-        $EARTH_RADIUS = 6370.996; // 地球半径系数
-        $PI = 3.1415926;
-        $radLat1 = $latitude1 * $PI / 180.0;
-        $radLat2 = $latitude2 * $PI / 180.0;
-        $radLng1 = $longitude1 * $PI / 180.0;
-        $radLng2 = $longitude2 * $PI / 180.0;
-        $a = $radLat1 - $radLat2;
-        $b = $radLng1 - $radLng2;
-        $distance = 2 * asin(sqrt(pow(sin($a / 2), 2) + cos($radLat1) * cos($radLat2) * pow(sin($b / 2), 2)));
-        $distance = $distance * $EARTH_RADIUS * 1000;
-        if ($unit == 2) {
-            $distance = $distance / 1000;
-        }
-        return round($distance, $decimal);
-//        $shops = Shop::with('user')->orderBy('id', 'desc')->paginate(9);
-//        return new ShopCollection($shops);
+        $shops = Shop::with('user')->orderBy('id', 'desc')->paginate(9);
+        return new ShopCollection($shops);
     }
 
     public function show($id)
--- app/Http/Resources/Shop.php
+++ app/Http/Resources/Shop.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Dingo\Api\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class Shop extends JsonResource
@@ -14,6 +15,26 @@ class Shop extends JsonResource
      */
     public function toArray($request)
     {
+        $unit = 2;
+        $decimal = 0;
+        $latitude1 = $request->latitude;
+        $latitude2 = $this->latitude;
+        $longitude1 = $request->longitude;
+        $longitude2 = $this->longitude;
+        $EARTH_RADIUS = 6370.996; // 地球半径系数
+        $PI = 3.1415926;
+        $radLat1 = $latitude1 * $PI / 180.0;
+        $radLat2 = $latitude2 * $PI / 180.0;
+        $radLng1 = $longitude1 * $PI / 180.0;
+        $radLng2 = $longitude2 * $PI / 180.0;
+        $a = $radLat1 - $radLat2;
+        $b = $radLng1 - $radLng2;
+        $distance = 2 * asin(sqrt(pow(sin($a / 2), 2) + cos($radLat1) * cos($radLat2) * pow(sin($b / 2), 2)));
+        $distance = $distance * $EARTH_RADIUS * 1000;
+        if ($unit == 2) {
+            $distance = $distance / 1000;
+        }
+
         return [
             'id'=>$this->id,
             'userid'=>$this->user_id,
@@ -29,7 +50,8 @@ class Shop extends JsonResource
             'dynamic_count'=>$this->dynamic_count,
             'published_at'=>$this->published_at,
             'code'=>$this->code,
-            'user'=>new User($this->user)
+            'user'=>new User($this->user),
+            'distance'=>round($distance, $decimal)
         ];
     }
 }
