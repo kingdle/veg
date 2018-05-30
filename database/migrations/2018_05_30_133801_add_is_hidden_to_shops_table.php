b4a91c3c637ac78c81db8093cd314fc49446fc43
kingdle <nkings@163.com>
2018-05-30 13:43:24 +0800

    update shop is_hidden

---
+++ database/migrations/2018_05_30_133801_add_is_hidden_to_shops_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddIsHiddenToShopsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('shops', function (Blueprint $table) {
+            $table->integer('comments_count')->default(0)->after('code')->comment('评论数');
+            $table->integer('followers_count')->default(1)->after('comments_count')->comment('点赞数');
+            $table->integer('answers_count')->default(0)->after('followers_count')->comment('回答数');
+            $table->string('close_comment',8)->default('F')->after('answers_count')->comment('是否关闭评论');
+            $table->string('is_hidden',8)->default('F')->after('close_comment')->comment('是否隐藏动态');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('shops', function (Blueprint $table) {
+            $table->dropColumn('comments_count');
+            $table->dropColumn('followers_count');
+            $table->dropColumn('answers_count');
+            $table->dropColumn('close_comment');
+            $table->dropColumn('is_hidden');
+        });
+    }
+}
--- resources/assets/js/components/confirm/Email.vue
+++ resources/assets/js/components/confirm/Email.vue
@@ -4,11 +4,11 @@
             <div class="col-md-8">
                 <div class="card card-default">
                     <div class="card-header">
-                        <h4>新注册商户需要上传简单资料就可以发动态了</h4>
+                        <h4>商户电脑端需要上传营业执照、法人身份证电子版就可以使用订单管理功能了</h4>
                     </div>
 
                     <div class="card-body">
-                        <strong>商户未通过认证前好多服务用不了哦！农户买东西不受影响哈。</strong>
+                        <strong>商户未通过认证前好多服务用不了！农户订苗不受影响。</strong>
                     </div>
                 </div>
             </div>
