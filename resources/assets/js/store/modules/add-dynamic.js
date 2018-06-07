5560a7892459cc0feab8779e25c60bbbfb8bba04
kingdle <nkings@163.com>
2018-06-07 22:18:06 +0800

    update password removeToken

--- resources/assets/js/store/modules/add-dynamic.js
+++ resources/assets/js/store/modules/add-dynamic.js
@@ -1,11 +1,12 @@
 export default {
     actions: {
         addDynamicRequest({dispatch}, formData) {
+            var myDate = new Date();
             return axios.post('api/v1/news/addnews', formData).then(response => {
                 console.log(response.data)
-                dispatch('showNotification', {level: 'success', msg: '动态发布成功'})
+                dispatch('showNotification', {level: 'success', msg: '动态发布成功'+myDate.toLocaleTimeString()})
             }).catch(errors => {
-                dispatch('showNotification', {level: 'error', msg: '动态发布失败'})
+                dispatch('showNotification', {level: 'error', msg: '动态发布失败'+myDate.toLocaleTimeString()})
             })
         },
     }
--- resources/assets/js/store/modules/edit-password.js
+++ resources/assets/js/store/modules/edit-password.js
@@ -1,12 +1,16 @@
+import jwtToken from './../../helpers/jwt'
+
 export default {
     actions: {
         updatePasswordRequest({dispatch}, formData) {
+            var myDate = new Date();
             return axios.post('/api/user/password/update', formData).then(response => {
                 $('#PasswordModalCenter').modal('hide')
-                dispatch('showNotification', {level: 'success', msg: '更新密码成功'})
+                jwtToken.removeToken()
+                dispatch('showNotification', {level: 'success', msg: '更新密码成功'+myDate.toLocaleTimeString()})
             }).catch(errors => {
                 $('#PasswordModalCenter').modal('hide')
-                dispatch('showNotification', {level: 'error', msg: '更新密码失败'})
+                dispatch('showNotification', {level: 'error', msg: '更新密码失败'+myDate.toLocaleTimeString()})
             })
         },
     }
