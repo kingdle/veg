9357aa4b3242b1d50ef519fc327aec0b8760c14b
kingdle <nkings@163.com>
2018-06-23 00:59:35 +0800

    orders buyerConfirm API

--- app/Order.php
+++ app/Order.php
@@ -6,7 +6,39 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    protected $fillable=['user_id','to_user_id','prod_id','tag_id','counts','price','unit_price','total_price','name','address','cityInfo','villageInfo','longitude','latitude','phone','state','state_at','payment','payment_at','start_at','end_at','note_buy','rate_buyer','note_sell','rate_seller','is_del','deleted_at'];
+    protected $fillable=[
+        'user_id',
+        'to_user_id',
+        'prod_id',
+        'tag_id',
+        'counts',
+        'price',
+        'unit_price',
+        'total_price',
+        'name',
+        'address',
+        'cityInfo',
+        'villageInfo',
+        'longitude',
+        'latitude',
+        'phone',
+        'state',
+        'state_at',
+        'payment',
+        'payment_at',
+        'is_confirm',
+        'buyer_at',
+        'seller_at',
+        'buyer_confirm_at',
+        'start_at',
+        'end_at',
+        'note_buy',
+        'rate_buyer',
+        'note_sell',
+        'rate_seller',
+        'is_del',
+        'deleted_at'
+    ];
     public function user(){
         return $this->belongsTo('App\User','user_id');
     }
