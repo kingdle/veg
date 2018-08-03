5eafb25ae4cbefaaf191ab52dcfab2559a1b40e7
kingdle <nkings@163.com>
2018-08-03 16:02:16 +0800

    update shop model

--- app/Shop.php
+++ app/Shop.php
@@ -6,7 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Shop extends Model
 {
-    protected $fillable =['user_id','title','summary','content','property','avatar','pic_count','dynamic_count','click_count','address','published_at','code','longitude','latitude','cityInfo','villageInfo'];
+    protected $fillable =[
+        'user_id','title','summary','content','property',
+        'legal_person','legal_person_id','legal_person_id_card','business_license','business_license_pic',
+        'avatar','pic_count','dynamic_count','click_count',
+        'address','published_at','code','longitude','latitude',
+        'language','country','province','cityInfo','villageInfo'];
 
     public function user(){
         return $this->belongsTo('App\User','user_id');
