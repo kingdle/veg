f7cd3a1778271add96dee5ae060d3872c6787bd9
kingdle <nkings@163.com>
2018-08-05 15:32:48 +0800

    prod get

--- app/Http/Controllers/ProdsController.php
+++ app/Http/Controllers/ProdsController.php
@@ -19,7 +19,7 @@ class ProdsController extends Controller
     public function prodSeller($id)
     {
 //        $shopId = Auth::guard('api')->user()->shop->id;
-        $prods = Prod::where('shop_id',$id)->where('title','not like','%克隆%')->where('title','not like','%代育%')->where('title','not like','%拷贝%')->orderBy('likes_count', 'desc')->paginate(9);
+        $prods = Prod::where('shop_id',$id)->where('title','not like','%克隆%')->where('title','not like','%代育%')->where('title','not like','%拷贝%')->orderBy('likes_count', 'desc')->get();
         return new ProdCollection($prods);
     }
     public function show($id)
