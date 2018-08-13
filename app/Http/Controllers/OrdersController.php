aa7c9d22f28458ec7a8cd4b55299463d4ed78604
kingdle <nkings@163.com>
2018-08-13 13:52:31 +0800

    1.3.2 regist bug

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -56,10 +56,11 @@ class OrdersController extends Controller
         $userId = Auth::guard('api')->user()->id;
         $phone = $request->phone;
         $orders = Order::where("phone",'=',$phone)->with(['shop'=>function($query){
-            $query->select('id','title');
+            $query->select('id','title','phone');
         }])->with(['prod'=>function($query){
             $query->select('id','sort_id','title','pic');
         }])->where('is_del', '=', 'F')->orderby("end_at","desc")->get();
+        $orderCounts=Order::latest()->where("phone",'=',$phone)->where('is_del', '=','F')->count();//订单总数
         if ($orders->count() == 0) {
             $data['status'] = false;
             $data['status_code'] = '401';
@@ -85,6 +86,7 @@ class OrdersController extends Controller
                 'counts' => $value->counts,
                 'payment' => $value->payment,
                 'state' => $value->state,
+                'counts'=>$orderCounts,
                 'end_at' => substr($value->end_at,0,10),
                 'created_at' => substr($value->created_at,0,10),
             ];
