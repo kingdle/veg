91212b344d3a76051a6dc9463ca8f4a2f82d8725
kingdle <nkings@163.com>
2018-08-20 13:31:28 +0800

    update favorites shops followers_count

--- app/Http/Controllers/Api/FavoritesController.php
+++ app/Http/Controllers/Api/FavoritesController.php
@@ -31,12 +31,14 @@ class FavoritesController extends Controller
     public function store(Request $request){
         $userId=Shop::where('id',$request->get('shop_id'))->first()->user_id;
         User::where('id', $userId)->increment('followings_count');//关注用户的被关注数加1//increment自增decrement自减
-        User::where('id', Auth::user()->id)->increment('followers_count');//关注数加1
+        User::where('id', Auth::user()->id)->increment('followers_count');//用户关注数加1
+        Shop::where('id',$request->get('shop_id'))->increment('followers_count');//苗厂关注数加1
         Auth::user()->favorites()->attach($request->get('shop_id'));
         return response()->json([
             'status'=>'true',
             'status_code' => 200,
-            'message' => '苗场收藏成功'
+            'message' => '苗场收藏成功',
+            'data' => ''
         ]);
     }
     public function destroy($id){
@@ -44,10 +46,12 @@ class FavoritesController extends Controller
         Auth::user()->favorites()->detach($id);
         User::where('id', $userId)->decrement('followings_count');//关注用户的被关注数减1//increment自增decrement自减
         User::where('id', Auth::user()->id)->decrement('followers_count');//关注数减1
+        Shop::where('id',$id)->decrement('followers_count');//苗厂关注数减1
         return response()->json([
             'status'=>'true',
-            'status_code' => 200,
-            'message' => '取消收藏成功'
+            'status_code' => 201,
+            'message' => '取消收藏成功',
+            'data' => ''
         ]);
     }
 }
