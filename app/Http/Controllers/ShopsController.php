80b2c468cad82eeea093fe37feaa75daf6b740d0
kingdle <nkings@163.com>
2018-08-21 17:19:10 +0800

    update query shop

--- app/Http/Controllers/ShopsController.php
+++ app/Http/Controllers/ShopsController.php
@@ -49,7 +49,7 @@ class ShopsController extends Controller
         }
         $shops = Shop::where("title",'like',$queryText)->orwhere("summary",'like',$queryText)->orwhere("address",'like',$queryText)->where("is_hidden",'!=','T')->where("is_service",'!=','T')
             ->selectRaw('id,user_id,summary,title,avatar,province,cityInfo,address,villageInfo,code,longitude,latitude,dynamic_count,pic_count,acos(cos(' . $lat . '*pi()/180 )*cos(latitude*pi()/180)*cos(' . $lng . '*pi()/180 -longitude*pi()/180)+sin(' . $lat . '*pi()/180 )*sin(latitude*pi()/180))*6370996.81  as distance')  //使用原生sql
-            ->orderby("distance","asc")->get();
+            ->orderby("distance","asc")->paginate(20);
         return new ShopCollection($shops);
     }
 
