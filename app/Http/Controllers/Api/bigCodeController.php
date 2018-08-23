1bfc0102320dba34bd6b16653f88ac7fcfd47f0f
kingdle <nkings@163.com>
2018-08-23 11:54:50 +0800

    add bigCode update

--- app/Http/Controllers/Api/bigCodeController.php
+++ app/Http/Controllers/Api/bigCodeController.php
@@ -2,19 +2,20 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Config;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class bigCodeController extends Controller
 {
     public function index(){
-        $blue=Config::find('5');
+        $bigCode=Config::find('5');
         return response()->json([
             'status'=>'true',
             'status_code' => 200,
             'message' => '信息获取成功',
-            'data'=>$blue,
-            'url'=>json_decode($blue->slide)
+            'data'=>$bigCode,
+            'url'=>json_decode($bigCode->slide)
         ]);
     }
 }
